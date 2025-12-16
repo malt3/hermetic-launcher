63b6befd9a9f3e9317963458ec5a9a2c9a9fc25e
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-12-16 14:55:27 +0100

    bcr: add starlark docs

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -21,6 +21,15 @@ tools/create-release-archive/target/release/create-release-archive \
 
 echo "Archive created at dist/hermetic_launcher-${TAG}.tar.gz" >&2
 
+echo "Packaging Starlark docs..." 1>&2
+# Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
+docs="$(mktemp -d)"; targets="$(mktemp)"
+bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" build --target_pattern_file="$targets" --remote_download_regex='.*doc_extract\.binaryproto'
+tar --create --auto-compress \
+    --directory "$(bazel --output_base="$docs" info bazel-bin)" \
+    --file "dist/hermetic_launcher-${TAG}.docs.tar.gz" .
+
 # Output release notes to STDOUT (this will be used as the GitHub release body)
 cat << EOF
 ## Hermetic Launcher Bazel Rules ${TAG}
@@ -32,6 +41,6 @@ This release contains the Bazel rules for hermetic_launcher.
 Add to your \`MODULE.bazel\`:
 
 \`\`\`starlark
-bazel_dep(name = "hermetic_launcher", version = "${TAG}")
+bazel_dep(name = "hermetic_launcher", version = "${TAG#v}")
 \`\`\`
 EOF
